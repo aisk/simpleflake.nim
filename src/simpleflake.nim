2ad623f921e6bed1b0accb4299d5930370a0b184
An Long <aisk1988@gmail.com>
2021-01-04 21:59:21 +0800

    🐛 don't using global rand state

--- src/simpleflake.nim
+++ src/simpleflake.nim
@@ -6,10 +6,12 @@ const SIMPLEFLAKE_EPOCH = 946702800
 const SIMPLEFLAKE_RANDOM_LENGTH = 23
 
 random.randomize()
+let now = times.getTime()
+var rand = random.initRand(now.toUnix * 1_000_000_000 + now.nanosecond)
 
 proc gen*(): uint64 =
   let now = times.getTime().toUnixFloat()
   var ts = uint64((now - SIMPLEFLAKE_EPOCH) * 1000)
-  let randomBits = uint64(random.rand(2 ^ SIMPLEFLAKE_RANDOM_LENGTH))
+  let randomBits = uint64(rand.rand(2 ^ SIMPLEFLAKE_RANDOM_LENGTH))
 
   return ts shl SIMPLEFLAKE_RANDOM_LENGTH or randomBits
