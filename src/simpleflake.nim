09023f10738c836fc3d140a6ee888199e04419f6
AN Long <aisk1988@gmail.com>
2023-06-01 23:40:02 +0800

    ✨ support parse simpleflake

--- src/simpleflake.nim
+++ src/simpleflake.nim
@@ -4,6 +4,7 @@ import times
 
 const SIMPLEFLAKE_EPOCH = 946702800
 const SIMPLEFLAKE_RANDOM_LENGTH = 23
+const SIMPLEFLAKE_TIMESTAMP_LENGTH = 41
 
 let now = times.getTime()
 var rand = random.initRand(now.toUnix * 1_000_000_000 + now.nanosecond)
@@ -14,3 +15,9 @@ proc gen*(): uint64 =
   let randomBits = uint64(rand.rand(2 ^ SIMPLEFLAKE_RANDOM_LENGTH))
 
   return ts shl SIMPLEFLAKE_RANDOM_LENGTH or randomBits
+
+proc parse*(flake: uint64): tuple[timestamp: float, randomBits: uint64] =
+  let timestamp: float = float(flake shr SIMPLEFLAKE_RANDOM_LENGTH) / 1000 + SIMPLEFLAKE_EPOCH
+  let bitmask = (1u64 shl SIMPLEFLAKE_RANDOM_LENGTH) - 1
+  let randomBits: uint64 = flake and bitmask
+  return (timestamp: timestamp, randomBits: randomBits)
--- tests/tests.nim
+++ tests/tests.nim
@@ -4,3 +4,8 @@ import simpleflake
 
 test "can generate simpleflake id":
   check gen() > uint64(5432510521311235617)
+
+test "can parse simpleflake id":
+  let parsed = simpleflake.parse(5432510521311235617u64)
+  check parsed.timestamp == 1594308519.7220001
+  check parsed.randomBits == 5508641
